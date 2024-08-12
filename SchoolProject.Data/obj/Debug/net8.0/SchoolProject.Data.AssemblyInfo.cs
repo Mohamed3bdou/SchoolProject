bc0d930137c8804f01516f7c19d23093c4915954
Mohamed 3bdou <mohamed.3bdoo.official2021@gmail.com>
2024-08-12 10:22:31 +0300

    Add Project References

--- SchoolProject.Data/obj/Debug/net8.0/SchoolProject.Data.AssemblyInfo.cs
+++ SchoolProject.Data/obj/Debug/net8.0/SchoolProject.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolProject.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16873762466f067cf2b93e1630019926d0f4c2fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolProject.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolProject.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
